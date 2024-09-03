344722a85b24149178b3f3216335a53475f852b9
srikarar <srikarar@gmail.com>
2024-09-03 15:19:29 +0530

    added meta data

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../assets/img/logo.png';
+
+const Header = () => {
+  return (
+    <header>
+        Header
+        <img src={logo} />
+    </header>
+  )
+}
+
+export default Header
